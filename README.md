e067f40b081d82fed433e177df228b1da8b687e0
vjaceslavs <v.krivosejevs@icloud.com>
2025-01-06 10:26:33 +0200

    make base for final project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stepik_4_public
+lesson 4.1. and 4.2. practice
---
+++ tests/login_page.py
@@ -0,0 +1,22 @@
+from tests.base_page import BasePage
+
+from tests.locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # Check id url is correct
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"Expected 'login' to be in URL, but got {current_url}"
+
+    def should_be_login_form(self):
+        # check if login form is presented
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # check if registration form is presented
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON), "Register form is not presented"
