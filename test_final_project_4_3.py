6a300dacf4b96f344dca2c3a40e7543d3e1e4d8f
vjaceslavs <v.krivosejevs@icloud.com>
2025-01-09 16:39:07 +0200

    test basket open from product page

--- test_final_project_4_3.py
+++ test_final_project_4_3.py
@@ -1,25 +1,11 @@
 import time
 import pytest
 
-from pages.base_page import BasePage
 from pages.basket_page import BasketPage
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
-
-
-urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  # pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  #   marks=pytest.mark.xfail(reason="Known bug on this promo page")),
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-       ]
+from urls import urls
 
 @pytest.mark.skip(reason="Test is ready")
 @pytest.mark.parametrize('link', urls)
